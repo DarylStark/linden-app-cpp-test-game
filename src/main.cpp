f213e7d00c44d8b08841fa9c37b18556799c6eb1
Daryl Stark <daryl@dstark.nl>
2024-11-08 21:06:53 +0000

    Test with different renderer

--- src/main.cpp
+++ src/main.cpp
@@ -13,27 +13,21 @@ int main()
     // Create a window
     linden::graphics::SDL2Window w = sdl2.create_window(
         "SDL2 Basic Example", {SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED},
-        {1024, 768});
+        {800, 800});
 
     SDL_Window* window = w.get_window_handle();
-
-    // Create a renderer
-    SDL_Renderer* renderer =
-        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
-    {
-        std::cerr << "Failed to create renderer: " << SDL_GetError()
-                  << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+    SDL_Renderer* renderer = w.get_renderer_handle();
 
     // Main loop flag
     bool quit = false;
 
     // Event handler
     SDL_Event e;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
+    SDL_RenderClear(renderer);
+    int32_t x = 0;
+    int32_t y = 0;
 
     while (!quit)
     {
@@ -52,6 +46,15 @@ int main()
         // Clear screen
         SDL_SetRenderDrawColor(renderer, 0x00, 0x66, 0x00, 0xff);
         SDL_RenderClear(renderer);
+
+        // Draw a pixel
+        SDL_SetRenderDrawColor(renderer, 0, 0x0, 0, 255);
+        SDL_RenderDrawPoint(renderer, ++x, ++y);
+
+        for (int i = -25; i < 25; i++)
+        {
+            SDL_RenderDrawPoint(renderer, x + i, y - i);
+        }
 
         // Update screen
         SDL_RenderPresent(renderer);
