1acf3959f8a508d2e3c4ad37e4f39c9f836e0a5b
Daryl Stark <daryl@dstark.nl>
2024-11-11 10:17:52 +0000

    Add images for the textures

--- src/main.cpp
+++ src/main.cpp
@@ -15,83 +15,18 @@ int main()
     // Create a window
     linden::graphics::SDL2Window w = sdl2.create_window(
         "SDL2 Basic Example", {SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED},
-        {1920, 1080});
+        {1024 * 2, 1024 * 2});
 
     SDL_Window* window = w.get_sdl2_window_handle();
 
-    // -----------------------------------------------------------
-    // Create a green car
-    linden::graphics::SDL2StreamingTexture t_green(*w.get_renderer(), {7, 12});
-    SDL_Texture* texture_green = t_green.get_sdl2_texture_handle();
+    // Load the texture for the background
+    linden::graphics::SDL2ImageTexture t_bg(*w.get_renderer(), "assets/bg.jpg");
+    SDL_Texture* texture_bg = t_bg.get_sdl2_texture_handle();
 
-    // Add data to the texture
-    t_green.lock();
-
-    // Fill the texture with a simple pattern
-    for (int32_t x = 0; x < 7; x++)
-    {
-        for (int32_t y = 0; y < 12; y++)
-        {
-            t_green.set_pixel({x, y}, {0, 0, 0, 0});
-        }
-    }
-    t_green.set_pixel({0, 2}, {0, 0x66, 0, 255});
-    t_green.set_pixel({6, 2}, {0, 0x66, 0, 255});
-    t_green.set_pixel({0, 3}, {0, 0x66, 0, 255});
-    t_green.set_pixel({6, 3}, {0, 0x66, 0, 255});
-    t_green.set_pixel({0, 8}, {0, 0x66, 0, 255});
-    t_green.set_pixel({6, 8}, {0, 0x66, 0, 255});
-    t_green.set_pixel({0, 9}, {0, 0x66, 0, 255});
-    t_green.set_pixel({6, 9}, {0, 0x66, 0, 255});
-
-    for (int32_t y = 0; y <= 11; y++)
-    {
-        t_green.set_pixel({1, y}, {0, 0x66, 0, 128});
-        t_green.set_pixel({2, y}, {0, 0x66, 0, 128});
-        t_green.set_pixel({3, y}, {0, 0x66, 0, 128});
-        t_green.set_pixel({4, y}, {0, 0x66, 0, 128});
-        t_green.set_pixel({5, y}, {0, 0x66, 0, 128});
-    }
-    t_green.set_pixel({1, 0}, {0, 0, 0, 0});
-    t_green.set_pixel({5, 0}, {0, 0, 0, 0});
-    t_green.unlock();
-    // -----------------------------------------------------------
-    // Create a red car
-    linden::graphics::SDL2StreamingTexture t_red(*w.get_renderer(), {7, 12});
-    SDL_Texture* texture_red = t_red.get_sdl2_texture_handle();
-
-    // Add data to the texture
-    t_red.lock();
-
-    // Fill the texture with a simple pattern
-    for (int32_t x = 0; x < 7; x++)
-    {
-        for (int32_t y = 0; y < 12; y++)
-        {
-            t_red.set_pixel({x, y}, {0, 0, 0, 0});
-        }
-    }
-    t_red.set_pixel({0, 2}, {0xff, 0, 0, 255});
-    t_red.set_pixel({6, 2}, {0xff, 0, 0, 255});
-    t_red.set_pixel({0, 3}, {0xff, 0, 0, 255});
-    t_red.set_pixel({6, 3}, {0xff, 0, 0, 255});
-    t_red.set_pixel({0, 8}, {0xff, 0, 0, 255});
-    t_red.set_pixel({6, 8}, {0xff, 0, 0, 255});
-    t_red.set_pixel({0, 9}, {0xff, 0, 0, 255});
-    t_red.set_pixel({6, 9}, {0xff, 0, 0, 255});
-
-    for (int32_t y = 0; y <= 11; y++)
-    {
-        t_red.set_pixel({1, y}, {0xff, 0, 0, 128});
-        t_red.set_pixel({2, y}, {0xff, 0, 0, 128});
-        t_red.set_pixel({3, y}, {0xff, 0, 0, 128});
-        t_red.set_pixel({4, y}, {0xff, 0, 0, 128});
-        t_red.set_pixel({5, y}, {0xff, 0, 0, 128});
-    }
-    t_red.set_pixel({1, 0}, {0, 0, 0, 0});
-    t_red.set_pixel({5, 0}, {0, 0, 0, 0});
-    t_red.unlock();
-    // -----------------------------------------------------------
+    // Load the texture for the car
+    linden::graphics::SDL2ImageTexture t_image(*w.get_renderer(),
+                                               "assets/car.png");
+    SDL_Texture* texture_image = t_image.get_sdl2_texture_handle();
 
     // Main loop flag
     bool quit = false;
@@ -102,8 +37,8 @@ int main()
     SDL_Rect rect;
     rect.x = 0;
     rect.y = 0;
-    rect.w = 70;
-    rect.h = 120;
+    rect.w = 348 / 3;
+    rect.h = 685 / 3;
     int32_t rot = 0;
     uint32_t speed = 15;
     uint8_t alpha = 255;
@@ -116,7 +51,6 @@ int main()
     bool right_pressed = false;
     bool space_pressed = false;
 
-    std::array<linden::graphics::SDL2Texture*, 2> textures = {&t_green, &t_red};
     uint32_t current_texture = 0;
 
     auto start = std::chrono::high_resolution_clock::now();
@@ -125,8 +59,6 @@ int main()
     {
         linden::utils::ScopedTimer timer;
         linden::utils::FrameRateLimiter limiter(60);
-
-        linden::graphics::SDL2Texture* texture = textures[current_texture % 2];
 
         while (SDL_PollEvent(&e) != 0)
         {
@@ -168,14 +100,14 @@ int main()
                             alpha -= 10;
                         else
                             alpha = 0;
-                        texture->set_alpha(alpha);
+                        t_image.set_alpha(alpha);
                         break;
                     case SDLK_EQUALS:
                         if (alpha < 255 - 10)
                             alpha += 10;
                         else
                             alpha = 255;
-                        texture->set_alpha(alpha);
+                        t_image.set_alpha(alpha);
                         break;
                 }
             }
@@ -223,23 +155,15 @@ int main()
         {
             const int new_x = rect.x + (speed * cos((rot - 90) * M_PI / 180.0));
             const int new_y = rect.y + (speed * sin((rot - 90) * M_PI / 180.0));
-            if (new_x >= 0 && new_x <= 1920 - 70 && new_y >= 0 &&
-                new_y <= 1080 - 120)
-            {
-                rect.x = new_x;
-                rect.y = new_y;
-            }
+            rect.x = new_x;
+            rect.y = new_y;
         }
         if (down_pressed)
         {
             const int new_x = rect.x - (speed * cos((rot - 90) * M_PI / 180.0));
             const int new_y = rect.y - (speed * sin((rot - 90) * M_PI / 180.0));
-            if (new_x >= 0 && new_x <= 1920 - 70 && new_y >= 0 &&
-                new_y <= 1080 - 120)
-            {
-                rect.x = new_x;
-                rect.y = new_y;
-            }
+            rect.x = new_x;
+            rect.y = new_y;
         }
         if (left_pressed)
         {
@@ -250,15 +174,17 @@ int main()
             rot += 5;
         }
 
-        // Set background color
-        SDL_SetRenderDrawColor(w.get_renderer()->get_sdl2_renderer_handle(),
-                               0xcc, 0xcc, 0xcc, 0xff);
+        // Clear screen
         SDL_RenderClear(w.get_renderer()->get_sdl2_renderer_handle());
+
+        // Set background
+        SDL_RenderCopy(w.get_renderer()->get_sdl2_renderer_handle(), texture_bg,
+                       nullptr, nullptr);
 
         // Render the texture
         SDL_RenderCopyEx(w.get_renderer()->get_sdl2_renderer_handle(),
-                         texture->get_sdl2_texture_handle(), nullptr, &rect,
-                         rot, nullptr, SDL_FLIP_NONE);
+                         t_image.get_sdl2_texture_handle(), nullptr, &rect, rot,
+                         nullptr, SDL_FLIP_NONE);
 
         // Render the texture
         SDL_RenderPresent(w.get_renderer()->get_sdl2_renderer_handle());
